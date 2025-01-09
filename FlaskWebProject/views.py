f5b63ba806a730b47956cf6edd50f9561c95f034
Magid <magid.zaafran@chevron.com>
2025-01-09 14:14:26 -0600

    Update views.py
    https

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -164,7 +164,7 @@ def authorized():
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
-            redirect_uri=url_for('authorized', _external=True, _scheme='http'))
+            redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
@@ -209,4 +209,4 @@ def _build_auth_url(authority=None, scopes=None, state=None):
     return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes or [],
         state=state or str(uuid.uuid4()),
-        redirect_uri=url_for('authorized', _external=True, _scheme='http'))
+        redirect_uri=url_for('authorized', _external=True, _scheme='https'))
